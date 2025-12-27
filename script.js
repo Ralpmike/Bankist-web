660d64ebf81423fe571426f03c89422f79a22a8d
Ralpmike <ralphmike996@gmail.com>
2025-12-27 16:58:46 +0100

    Event propagation in practice

--- script.js
+++ script.js
@@ -131,3 +131,34 @@ btnScrollTo.addEventListener("click", function (e) {
   //? modern way
   section1.scrollIntoView({behavior: "smooth"})
 });
+
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min); 
+
+const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+console.log(randomColor());
+
+document.querySelector(".nav__link").addEventListener("click", function (e) {
+  e.preventDefault();
+  this.style.backgroundColor = randomColor();
+  console.log("LINK", e.target, e.currentTarget);
+  console.log(e.target === this);
+
+  //?Stop propagation
+  // e.stopPropagation();
+})
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+  this.style.backgroundColor = randomColor();
+  console.log("CONTAINER", e.target, e.currentTarget);
+  // console.log(e.currentTarget === this);
+})
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  e.preventDefault();
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
+}, true)
